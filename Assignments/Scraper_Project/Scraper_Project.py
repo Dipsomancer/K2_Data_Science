e157c9fa7b7be295e864b4da54a8d113087ce96d
Dipsomancer <urifeld1@bu.edu>
2018-06-05 17:11:34 -0400

    Wikipedia table creation done

--- Assignments/Scraper_Project/Scraper_Project.py
+++ Assignments/Scraper_Project/Scraper_Project.py
@@ -26,7 +26,7 @@ print('{county}'.format(**political)) #Review
 
 '''
 #Wikipedia
-'''
+
 import lxml
 import wikipedia
 import pandas as pd
@@ -53,8 +53,10 @@ for table_row in table.find_all('tr'):
         row_in_data.append(cell.find(text=True).strip().replace(u'\xa0',' '))
     new_york_table.append(row_in_data)
 
+new_york_table.pop(0)
+
 print(new_york_table)
-'''
+
 #SQL
 
 import sqlite3
@@ -64,7 +66,7 @@ import datetime
 #Create Wikipedia Table and User Entry Table
 
 currentDT = str(datetime.datetime.now())
-print(currentDT)
+
 # Connecting to the database file
 conn = sqlite3.connect('County_DB.sqlite') #put in with statement and then put in an except with a close at end
 c = conn.cursor()
@@ -75,8 +77,9 @@ c.execute('CREATE TABLE WIKIPEDIA_TABLE (County_Name TEXT PRIMARY KEY, FIPS_Code
 #User Entry Table
 c.execute('CREATE TABLE USER_ENTRY_TABLE (Entry_ID INTEGER PRIMARY KEY AUTOINCREMENT, User_Address TEXT, Entry_Timestamp Timestamp, County_Name TEXT, FIPS_Code TEXT, County_Seat TEXT, Created TEXT, Formed_From TEXT, Named_For TEXT,Density TEXT, Pop_2010 TEXT, Area TEXT)')
 
-#Practice Write
-c.execute("INSERT INTO WIKIPEDIA_TABLE (County_Name, FIPS_Code, County_Seat, Created, Formed_From, Named_For, Density, Pop_2010, Area) VALUES ('1', '2', '3', '4', '5', '6', '7', '8', '9')")
+#Write new_york_table from Wikipedia call into WIKIPEDIA_TABLE
+for r in new_york_table:
+    c.execute("INSERT INTO WIKIPEDIA_TABLE (County_Name, FIPS_Code, County_Seat, Created, Formed_From, Named_For, Density, Pop_2010, Area) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]))
 
 conn.commit()
 conn.close()
